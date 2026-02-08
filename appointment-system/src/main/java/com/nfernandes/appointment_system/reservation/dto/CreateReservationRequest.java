fff26305c2e1e85585c2857dfc8eb60510f367f2
Nico <nicolasfersou@gmail.com>
2026-02-08 11:42:01 +0100

    Implement ReservationController logic and validations

--- appointment-system/src/main/java/com/nfernandes/appointment_system/reservation/dto/CreateReservationRequest.java
+++ appointment-system/src/main/java/com/nfernandes/appointment_system/reservation/dto/CreateReservationRequest.java
@@ -1,4 +1,27 @@
 package com.nfernandes.appointment_system.reservation.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Getter
+@Setter
 public class CreateReservationRequest {
+
+    @NotNull
+    private Long ClientId;
+
+    @NotNull
+    private Long workerId;
+
+    @NotEmpty
+    private Set<Long> serviceIds;
+
+    @NotNull
+    private LocalDateTime startTime;
 }
